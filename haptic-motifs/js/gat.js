ce560d5dd406873a7b1512fe3fa5f2d01004a07b
tiny-martian <39358351+tiny-martian@users.noreply.github.com>
2020-06-26 14:56:37 -0600

    Update gat.js

--- haptic-motifs/js/gat.js
+++ haptic-motifs/js/gat.js
@@ -2,9 +2,10 @@ AFRAME.registerComponent('gat',{
   init: function () {
     console.log('Hello, World!');
   },
-  
+  /*
   tick: function (time, timeDelta) {
     var myElement = document.getElementById("video");
     console.log(myElement.currentTime);
   },
+  */
 });
