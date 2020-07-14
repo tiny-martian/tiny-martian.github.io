5595a79675318712546d227215de2a087859a15a
tiny-martian <39358351+tiny-martian@users.noreply.github.com>
2020-07-14 11:02:23 -0600

    Update get-angle-and-timecode.js

--- haptic-motifs/js/get-angle-and-timecode.js
+++ haptic-motifs/js/get-angle-and-timecode.js
@@ -1,4 +1,9 @@
 AFRAME.registerComponent('get-angle-and-timecode', {
+  schema: {
+    enabled: {default: true},
+    dx: {default: 2.0},
+    dy: {default: 2.0},
+  },
   init: function () {
     console.log('Heyo!');
   }
