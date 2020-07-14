eaf33314cbf7670af78fa323cc75cc17b624a8ee
tiny-martian <39358351+tiny-martian@users.noreply.github.com>
2020-07-14 10:55:47 -0600

    Update get-angle-and-timecode.js

--- haptic-motifs/js/get-angle-and-timecode.js
+++ haptic-motifs/js/get-angle-and-timecode.js
@@ -2,9 +2,4 @@ AFRAME.registerComponent('get-angle-and-timecode', {
   init: function () {
     console.log('Heyo!');
   }
-  
-  tick: function (time, timeDelta) {
-    var myElement = document.getElementById("video");
-    console.log(myElement.currentTime);
-  }
 });
